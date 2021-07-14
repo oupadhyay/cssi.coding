eb9fb16751e9472597a4e64e03a3006254489561
oupadhyay <46334571+oupadhyay@users.noreply.github.com>
2021-07-14 15:04:22 -0700

    interview-workshop

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        // Problem: sum of everything except X multiplied
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        compute(arr);
+    }
+
+    public static void compute(int[] arr) {
+        var sum = 0;
+        if (arr == null || arr.length == 1) {
+            sum = 0;
+        } else {
+            var total = 1;
+            // TODO: add about case with zeros within the array
+            for (var i = 0; i < arr.length; i++) {
+                total *= arr[i];
+            }
+            for (var j = 0; j < arr.length; j++) {
+                sum += total / arr[j];
+            }
+            System.out.println(sum);
+        }
+    }
+}
