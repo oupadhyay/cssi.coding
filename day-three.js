dd73874c5e3784dd9f4fcd7664410e9debdb64fa
oupadhyay <46334571+oupadhyay@users.noreply.github.com>
2021-07-14 17:51:04 -0700

    nits

--- day-three.js
+++ day-three.js
@@ -28,23 +28,17 @@ function draw() {
         lightMode();
     }
 
-
-    // The red and blue circles:
     fill(color1);
     ellipse(width * 1 / 4, height / 2, 50);
     fill(color2);
     ellipse(width * 3 / 4, height / 2, 50);
 
-    // The grey circle and the text:
     fill(textColor);
     ellipse(mouseX, mouseY, 50);
     text("Flip the switch (" + mouseX + ", " + mouseY + ")", 20, 20);
 }
 
 function drawCenterLine() {
-    // This function will turn stroke on, draw the line, and then turn stroke
-    // back off.
-    // Remember a line segment in p5.js has four arguments: x1, y1, x2, y2
     stroke(textColor);
     line(width / 2, 0, width / 2, height);
     noStroke();
