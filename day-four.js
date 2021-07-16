61da3dcfa3851cb1bb81939b8867b3377e4409e5
oupadhyay <46334571+oupadhyay@users.noreply.github.com>
2021-07-15 17:43:25 -0700

    cleanup

--- day-four.js
+++ day-four.js
@@ -1,67 +1,73 @@
-let brushHue, textColor, globalS, globalB, firacode, prevMouseX, prevMouseY, pPrevMouseY, pPrevMouseX;
-
+let paths = [];
+let painting = false;
+let next = 0;
+let current, previous;
+let firacode, textColor;
+let hue = 0;
+let globalS = 50;
+let globalB = 80;
 function preload() {
     firacode = loadFont("Fira-Code.ttf");
 }
 
 function setup() {
-    globalS = 50;
-    globalB = 80;
-    brushHue = 0;
     textColor = color(95);
-
     textFont(firacode, 24);
     createCanvas(windowWidth, windowHeight);
     colorMode(HSB, 360, 100, 100);
+    current = createVector(0, 0);
+    previous = createVector(0, 0);
     strokeWeight(6);
-    console.log("width: " + width + " height: " + height);
     noStroke();
     background(15);
 }
 
 function draw() {
-    fill(color(15));
-    rect(0, 0, width, height * 0.075);
-    rect(0, 0, width * 0.05, height);
-    chooseColors();
+    background(15);
+    if (millis() > next && painting) {
+        current.x = mouseX;
+        current.y = mouseY;
 
-    if (mouseIsPressed) {
+        let force = p5.Vector.sub(current, previous);
+        force.mult(0.05);
+        paths[paths.length - 1].add(current, force);
+        next = millis() + random(100);
 
+        previous.x = current.x;
+        previous.y = current.y;
+    }
 
-        if (prevMouseX === undefined || prevMouseY === undefined) {
-            pPrevMouseX = pmouseX;
-            pPrevMouseY = pmouseY;
-            prevMouseX = mouseX;
-            prevMouseY = mouseY;
-        } else {
-            pPrevMouseX = pmouseX;
-            pPrevMouseY = pmouseY;
-            prevMouseX = mouseX;
-            prevMouseY = mouseY;
-            strokeWeight(4 / (1 + Math.pow(Math.E, -(abs(pmouseX - mouseX) + abs(pmouseY - mouseY)))));
-            curve(pPrevMouseX, pPrevMouseY, prevMouseX, prevMouseY, pmouseX, pmouseY, mouseX, mouseY,);
-            // line(pmouseX, pmouseY, mouseX, mouseY);
-            // ellipse(mouseX, mouseY, abs(pmouseX - mouseX), abs(pmouseY - mouseY));
-        }
+    for (let p of paths) {
+        p.update();
+        p.display();
     }
 
     fill(textColor);
     noStroke();
     textSize(Math.min(0.1 / 3 * width, 0.1 / 3 * height));
-    translate(width * 0.015, height * 0.05);
-    text("H: " + pad(brushHue, 3) + ", S: " + pad(globalS, 3) + ", B: " + pad(globalB, 3), 0, 0);
-    translate(0, height * 0.6);
-    rotate(PI / 2.0);
-    text("X: " + pad(mouseX, 4) + "  Y: " + pad(mouseY, 4), 0, 0);
+    if (paths.length > 0) {
+        translate(width * 0.015, height * 0.05);
+        console.log(paths[paths.length - 1].hue);
+        text("H: " + pad(paths[paths.length - 1].hue, 3) + ", S: " + pad(globalS, 3) + ", B: " + pad(globalB, 3), 0, 0);
+        translate(0, height * 0.6);
+        rotate(PI / 2.0);
+        text("X: " + pad(mouseX, 4) + "  Y: " + pad(mouseY, 4), 0, 0);
+    }
 }
 
-function chooseColors() {
-    brushHue += 1;
-    if (brushHue >= 360) {
-        brushHue = 0;
+function mousePressed() {
+    next = 0;
+    painting = true;
+    previous.x = mouseX;
+    previous.y = mouseY;
+    hue += millis() / 100;
+    if (hue >= 360) {
+        hue = 0;
     }
-    stroke(brushHue, globalS, globalB);
-    fill(brushHue, globalS, globalB);
+    paths.push(new Path(hue));
+}
+function mouseReleased() {
+    painting = false;
 }
 
 function keyPressed() {
@@ -91,12 +97,79 @@ function keyPressed() {
     return false;
 }
 
-// function mousePressed() {
-//   ellipse(random(width), random(height), 30, 30)
-// }
+class Path {
+    constructor (hue) {
+        this.particles = [];
+        this.hue = hue;
+    }
+
+    add(position, force) {
+        this.particles.push(new Particle(position, force, this.hue));
+    }
+
+    update() {
+        for (let p of this.particles) {
+            p.update();
+        }
+    }
+
+    getHue() {
+        let h = 0;
+        for (let p of this.particles) {
+            h = p.hue;
+        }
+        return h;
+    }
+
+    display() {
+        for (let i = this.particles.length - 1; i >= 0; i--) {
+            if (this.particles[i].lifespan <= 0) {
+                this.particles.splice(i, 1);
+            } else {
+                this.particles[i].display(this.particles[i + 1]);
+            }
+        }
+
+    }
+}
+
+class Particle {
+    constructor (position, force, hue) {
+        this.position = createVector(position.x, position.y);
+        this.velocity = createVector(force.x, force.y);
+        this.drag = 0.9;
+        this.lifespan = 255 * 1.5;
+        this.hue = hue;
+    }
+
+    update() {
+        this.position.add(this.velocity);
+        this.velocity.mult(this.drag);
+        this.lifespan -= 2.5;
+        this.hue--;
+    }
+
+    hue() {
+        return this.hue;
+    }
+
+    display(other) {
+        stroke(this.hue, this.lifespan, 15 + this.lifespan);
+        fill(this.hue, this.lifespan / 2, 15 + this.lifespan / 2);
+        ellipse(this.position.x, this.position.y, 3, 3)
+
+        if (other) {
+            line(this.position.x, this.position.y, other.position.x, other.position.y);
+        }
+    }
+}
 
 function pad(num, size) {
-    num = num.toString();
-    while (num.length < size) num = "0" + num;
+    if (num) {
+        num = num.toString();
+        num = parseInt(num).toString();
+        while (num.length < size) num = "0" + num;
+        while (num.length > size) num = num.slice(0, 3);
+    }
     return num;
 }
