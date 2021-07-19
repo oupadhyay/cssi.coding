d2c7cb6f6377dbaf1149f8013add82e9d5c955fb
oupadhyay <46334571+oupadhyay@users.noreply.github.com>
2021-07-19 16:50:15 -0700

    updates

--- js/day-six.js
+++ js/day-six.js
@@ -1,4 +1,4 @@
-let brushHue, backgroundColor, coinX, coinY, score, time, gameIsOver, hit;
+let brushHue, backgroundColor, coinX, coinY, score, time, gameIsOver, hit, resetButton, coinValue;
 
 function setup() {
     // Canvas & color settings
@@ -6,46 +6,72 @@ function setup() {
     colorMode(HSB, 360, 100, 100);
     brushHue = 0;
     backgroundColor = 95;
-    coinX = random(width);
-    coinY = random(height);
-    score = 0;
-    time = 1000;
-    gameIsOver = false;
+
+    // Initialize game.
+    resetGame();
+
+    resetButton = createButton('RESET GAME');
+    //resetButton.position(50, 120);
+    resetButton.mousePressed(resetGame);
 }
+
 
 function draw() {
     background(backgroundColor);
-    ellipse(coinX, coinY, 20);
-    ellipse(mouseX, mouseY, 20);
-    text(`Your score is ${score}`, 20, 20);
-    text(`Time remaining: ${time}`, 20, 40);
-    handleTime();
 
-    hit = collideCircleCircle(coinX, coinY, 20, mouseX, mouseY, 20);
-    if (hit) {
+    handleTime();
+    if (!gameIsOver) {
+        // ! <- negation (aka NOT) false -> true, true -> false
         handleCollision();
     }
+
+    // Draw the coin
+    ellipse(coinX, coinY, 20);
+
+    // Draw the cursor at the mouse position
+    ellipse(mouseX, mouseY, 20);
+
+    // Add text with the time remaining: 
+    text(`Your score is: ${score}`, 20, 20);
+    text(`Time remaining: ${time}`, 20, 40);
+    if (gameIsOver) {
+        text('Game Over', 20, 60);
+    }
+    text(`Colliding: ${hit}`, 20, 80);
 }
 
 function handleCollision() {
-    if (!gameIsOver) {
-        coinY = random(height);
-        coinX = random(width);
-        score += 1;
+    // We'll write code for what happens if your character hits a coin.
+    hit = collideCircleCircle(coinX, coinY, 20, mouseX, mouseY, 20);
+    if (hit) {
+        score += coinValue;
+        moveCoin();
     }
 }
 
 function handleTime() {
+    // We'll write code to handle the time.
     if (time > 0) {
-        if (time < 100) {
-            time -= 0.1;
-        } else if (time < 50) {
-            time -= 0.01;
-        } else {
-            time -= 1;
-        }
+        time -= 1;
     } else {
         gameIsOver = true;
-        text(`Game over`, 20, 60);
     }
+}
+
+function moveCoin() {
+    coinX = random(width);
+    coinY = random(height);
+    coinValue = floor(random(5)) + 1; // 0 ~ 4.999999
+
+}
+
+function resetGame() {
+    // Get random coordinates for the starting position of the coin (coinX, coinY)
+    moveCoin();
+
+    time = 1000;
+    gameIsOver = false;
+    hit = false;
+    score = 0;
+    coinValue = 1;
 }
